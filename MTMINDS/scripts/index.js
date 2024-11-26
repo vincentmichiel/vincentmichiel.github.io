6f0b8820fdee4bd6a32a490cbbe388e6e5233748
Vincent Van den Broeck <vincent.vandenbroeck@student.hku.nl>
2024-11-26 10:45:36 +0100

    test

--- MTMINDS/scripts/index.js
+++ MTMINDS/scripts/index.js
@@ -1,18 +1,37 @@
 Paddle.Environment.set("sandbox");
-  Paddle.Initialize({ 
-    token: "test_576a670da1c59a1894c7202c319" // replace with a client-side token
-  });
-
-  let itemsList = [
-    {
-      priceId: "pri_01jdkwtf8wbh8cyjp4hcb4vydw",
-      quantity: 10
-    }
-  ];
-  
-  // open checkout
-  function openCheckout(items){
-    Paddle.Checkout.open({
-      items: items
-    });
+Paddle.Initialize({
+  token: "test_576a670da1c59a1894c7202c319", // replace with a client-side token
+  // prints events to console for debugging
+  eventCallback: function(data) {
+    console.log(data);
   }
+});
+
+// define items
+let itemsList = [
+  {
+    priceId: "pri_01jdkwtf8wbh8cyjp4hcb4vydw",
+    quantity: 10
+  },
+  {
+    priceId: "pri_01jdkxqzqwztrcntf6v941nm4a",
+    quantity: 1
+  }
+];
+
+// define customer details
+let customerInfo = {
+  email: "sam@example.com",
+  address: {
+    countryCode: "US",
+    postalCode: "10021"
+  }
+};
+
+// open checkout
+function openCheckout(items, customer) {
+  Paddle.Checkout.open({
+    items: items,
+    customer: customer
+  });
+}
